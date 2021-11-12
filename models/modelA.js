e7bd8d4e1d562e296cc1e4e6da90bec6e6c70189
Nicole Valentini <ncvalentini@gmail.com>
2021-11-12 12:15:52 -0700

    Create laptop object

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+var laptop = {
+  model: "Macbook Pro",
+  color: "granite",
+  batteryPercent: 100,
+  storageGb: 256,
+  isOn: true,
+  isWorking: true,
+  directories: ["turing", "photos", "desktop"],
+  applications: ["slack", "chrome", "iphoto",]
+}
+
+console.log(laptop);
