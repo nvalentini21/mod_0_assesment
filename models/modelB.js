1fd91a9f0fec45f0477b937c924f7bbb9615fd1d
Nicole Valentini <ncvalentini@gmail.com>
2021-11-12 12:26:09 -0700

    Create house object

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+var house = {
+  country: "United States",
+  name: "Nicole's House",
+  bedroomCount: 5,
+  bathroomCount: 2,
+  hasSolar: false,
+  isCozy: true,
+  amenities: ["television", "air conditioning", "washer", "dryer"],
+  occupants: ["Zack", "Nicole", "Jordan", "Mikasa", "Tammy"]
+}
+
+console.log(house);
