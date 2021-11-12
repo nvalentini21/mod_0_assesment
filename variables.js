f71b1ddb13b0e2575bc9e391d113094d1e38a8e3
Nicole Valentini <ncvalentini@gmail.com>
2021-11-12 12:06:44 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,14 @@
+var animal = "wolf";
+var isCuddly = false;
+animal = "tiger";
+
+var num8 = 8;
+var num3 = 3;
+var quotient = num8 / num3;
+
+var favoriteFilms = ["Chicago", "West Side Story", "Moulin Rouge"];
+favoriteFilms.shift();
+console.log(favoriteFilms);
+
+animal.length;
+console.log(animal.length);
